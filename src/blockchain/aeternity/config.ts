165ab6be67036707db852f522c94130a5ee4a81b
kraykov <raykov@weichain.tech>
2020-05-07 17:59:49 +0300

    Change the way blockchain config are initialized.

--- src/blockchain/aeternity/config.ts
+++ src/blockchain/aeternity/config.ts
@@ -1,19 +1,38 @@
 import { Config } from '@jelly-swap/aeternity';
-import AppConfig from '../../../config';
+import UserConfig from '../../config';
+import { safeAccess } from '../../utils';
+import { logError } from '../../logger';
 
-export default {
-    ...Config(7200),
-    providerUrl: 'https://sdk-mainnet.aepps.com/',
-    internalUrl: 'https://sdk-mainnet.aepps.com/',
-    compilerUrl: 'https://compiler.aepps.com',
-    wsUrl: 'wss://mainnet.aeternal.io/websocket',
-    apiUrl: 'https://mainnet.aeternal.io/',
-    contractAddress: 'ct_jmRkfpzmn7KZbXbkEL9wueJkb1vaFzMnVFJMFjAnJgj1CTtQe',
-    explorer: 'https://explorer.aepps.com/transactions/',
-    REFUND_PERIOD: 10,
-    //Expiration is in milliseconds
-    VALID_EXPIRATION: 72000000,
+export default () => {
+    const userConfig = new UserConfig().getUserConfig();
 
-    receiverAddress: AppConfig.BLOCKCHAIN.AE.ADDRESS,
-    KEY_PAIR: AppConfig.BLOCKCHAIN.AE.SECRET,
+    const address = safeAccess(userConfig, ['WALLETS', 'AE', 'ADDRESS']);
+    const secret = safeAccess(userConfig, ['WALLETS', 'AE', 'SECRET']);
+
+    const config = {
+        ...Config(7200),
+        providerUrl: 'https://sdk-mainnet.aepps.com/',
+        internalUrl: 'https://sdk-mainnet.aepps.com/',
+        compilerUrl: 'https://compiler.aepps.com',
+        wsUrl: 'wss://mainnet.aeternal.io/websocket',
+        apiUrl: 'https://mainnet.aeternal.io/',
+        contractAddress: 'ct_jmRkfpzmn7KZbXbkEL9wueJkb1vaFzMnVFJMFjAnJgj1CTtQe',
+        explorer: 'https://explorer.aepps.com/transactions/',
+        REFUND_PERIOD: 10,
+        //Expiration is in milliseconds
+        VALID_EXPIRATION: 72000000,
+    };
+
+    if (address && secret) {
+        return {
+            ...config,
+            receiverAddress: address,
+            KEY_PAIR: {
+                publicKey: userConfig.WALLETS['AE'].ADDRESS,
+                secretKey: userConfig.WALLETS['AE'].SECRET,
+            },
+        };
+    } else {
+        logError('Aeternity ADDRESS and SECRET are missing.');
+    }
 };
--- src/blockchain/ethereum/config.ts
+++ src/blockchain/ethereum/config.ts
@@ -1,14 +1,31 @@
 import { Config } from '@jelly-swap/ethereum';
-import AppConfig from '../../../config';
+import UserConfig from '../../config';
+import { safeAccess } from '../../utils';
+import { logError } from '../../logger';
 
-export default {
-    ...Config(7200),
-    providerUrl: 'https://mainnet.infura.io/v3/ee13a282868d4e7cb7d9a9543958631d',
-    contractAddress: '0x471B080EffB2bc6fb33c8c6FE6ce1AB46F9f522b',
-    explorer: 'https://etherscan.io/tx/',
-    REFUND_PERIOD: 10,
-    VALID_EXPIRATION: 72000,
-    gasMultiplier: 4,
-    receiverAddress: AppConfig.BLOCKCHAIN.ETH.ADDRESS,
-    PRIVATE_KEY: AppConfig.BLOCKCHAIN.ETH.SECRET,
+export default () => {
+    const userConfig = new UserConfig().getUserConfig();
+
+    const address = safeAccess(userConfig, ['WALLETS', 'ETH', 'ADDRESS']);
+    const secret = safeAccess(userConfig, ['WALLETS', 'ETH', 'SECRET']);
+
+    const config = {
+        ...Config(7200),
+        providerUrl: 'https://mainnet.infura.io/v3/ee13a282868d4e7cb7d9a9543958631d',
+        contractAddress: '0x471B080EffB2bc6fb33c8c6FE6ce1AB46F9f522b',
+        explorer: 'https://etherscan.io/tx/',
+        REFUND_PERIOD: 10,
+        VALID_EXPIRATION: 72000,
+        gasMultiplier: 4,
+    };
+
+    if (address && secret) {
+        return {
+            ...config,
+            receiverAddress: address,
+            PRIVATE_KEY: secret,
+        };
+    } else {
+        logError('Ethereums ADDRESS and SECRET are missing.');
+    }
 };
