3868bf69486bec565f308f2a38c88685d9ba2ebd
kraikov <raykov@weichain.tech>
2020-08-19 15:22:11 +0300

    Add User Withdraw for Harmony.

--- src/blockchain/harmony/index.ts
+++ src/blockchain/harmony/index.ts
@@ -1,5 +1,6 @@
 import { Contract } from '@jelly-swap/harmony';
 import { WalletProvider } from '@jelly-swap/harmony/dist/src/providers';
+import { greaterThan } from '../../utils/math';
 
 export default class EthereumContract extends Contract {
     private wallet: WalletProvider;
@@ -14,4 +15,14 @@ export default class EthereumContract extends Contract {
     // async signMessage(message: string) {
     // return await this.wallet.signMessage(message);
     // }
+
+    async userWithdraw(swap, secret) {
+        const address = swap.receiver;
+        const balance = await super.getBalance(address);
+        const isBalanceZero = greaterThan(balance, 0);
+        if (!isBalanceZero) {
+            const result = await super.withdraw({ ...swap, secret });
+            return result;
+        }
+    }
 }
