d47b3eaf6d2a19585e774b142046ed3e5d4a90d4
kraikov <raykov@weichain.tech>
2020-09-17 14:46:57 +0300

    Add custom price provider.

--- src/components/price/provider/index.ts
+++ src/components/price/provider/index.ts
@@ -1,7 +1,9 @@
 import CryptoCompareProvider from './cryptocompare';
 import BinanceProvider from './binance';
+import JellyProvider from './jelly';
 
 export default {
     CryptoCompare: CryptoCompareProvider,
     Binance: BinanceProvider,
+    Jelly: JellyProvider,
 };
