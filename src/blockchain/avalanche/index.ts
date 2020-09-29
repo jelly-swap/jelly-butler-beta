d5870fa11a51ac36d3f5fdba9f8f97518df081ff
kraikov <raykov@weichain.tech>
2020-09-29 16:37:44 +0300

    Override gas price and gas limit for Avalanche.

--- src/blockchain/avalanche/index.ts
+++ src/blockchain/avalanche/index.ts
@@ -3,6 +3,8 @@ import { WalletProvider } from '@jelly-swap/avalanche/dist/src/providers';
 
 import { greaterThan } from '../../utils/math';
 
+const DEFAULT_OPTIONS = { gasPrice: '0x6D6E2EDC00', gasLimit: 350000 };
+
 export default class EthereumContract extends Contract {
     private wallet: WalletProvider;
 
@@ -10,6 +12,18 @@ export default class EthereumContract extends Contract {
         const _wallet = new WalletProvider(config.PRIVATE_KEY, config);
         super(_wallet, config);
         this.wallet = _wallet;
+    }
+
+    async newContract(swap) {
+        return await super.newContract(swap, DEFAULT_OPTIONS);
+    }
+
+    async withdraw(withdraw) {
+        return await super.withdraw(withdraw, DEFAULT_OPTIONS);
+    }
+
+    async refund(refund) {
+        return await super.refund(refund, DEFAULT_OPTIONS);
     }
 
     async signMessage(message: string) {
@@ -22,7 +36,7 @@ export default class EthereumContract extends Contract {
         const isBalanceZero = greaterThan(balance, 0);
 
         if (!isBalanceZero) {
-            const result = await super.withdraw({ ...swap, secret });
+            const result = await super.withdraw({ ...swap, secret }, DEFAULT_OPTIONS);
             return result;
         }
     }
