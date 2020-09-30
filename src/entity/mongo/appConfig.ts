8d575fe42aee70ad2745b198286996bfbe53d574
AndonMitev <mitevandon94@gmail.com>
2020-09-30 10:54:05 +0300

    Add default types

--- src/entity/mongo/appConfig.ts
+++ src/entity/mongo/appConfig.ts
@@ -5,12 +5,18 @@ export default class AppConfig {
     @ObjectIdColumn()
     public id: ObjectID;
 
-    @Column()
+    @Column({
+        default: true,
+    })
     sound: boolean;
 
-    @Column()
+    @Column({
+        default: 'english',
+    })
     language: string;
 
-    @Column()
+    @Column({
+        default: 'dark_theme',
+    })
     theme: string;
 }
