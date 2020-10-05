afb2618236e1f040bdae460d209a7ca40b6f6025
AndonMitev <mitevandon94@gmail.com>
2020-10-05 10:14:33 +0300

    Remove commented code

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
 import { run } from './run';
 
-const config = process.argv[2];
-const combinedFile = process.argv[3];
-const errorFile = process.argv[4];
-
 // Electron fork
 run();
-
-// if (config) {
-//     run(JSON.parse(config), combinedFile, errorFile);
-// } else {
-
-// }
